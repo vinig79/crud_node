be65edb73cf6037a41796d37881a63507767bca2
vinic <viniciusgroto@gmail.com>
2023-04-12 01:09:58 -0300

    db online and new css

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize("node","vinig79","vinig79",{host:"localhost",dialect:"mysql"});
+const db = new Sequelize("sql10612503","sql10612503","UfHgGTvcIt",{host:"sql10.freemysqlhosting.net",dialect:"mysql"});
 
-module.exports = db
+module.exports = db;
--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,7 +1,17 @@
 function comentar(){
-    const text = document.querySelector('input[type=text]').value;
+    const text = document.getElementById('comment');
+    console.log(text);
     const dado = {text : text};
-    axios.post("/home", dado); 
+    //axios.post("/home", dado); 
     
-    window.location.href = "/index";
+    //window.location.href = "/index";
 }
+
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", e => {
+    textarea.style.height = "63px";
+    let scHeight = e.target.scrollHeight;
+    console.log(scHeight);
+    textarea.style.height = `${scHeight}px`;
+    
+});
