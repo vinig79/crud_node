4fb2bf450896678ebe306f67bcc56716966b8836
Pedrinnnnn <Pedrindbr2@gmail.com>
2023-04-12 19:40:50 -0300

    pack_vercel

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize("sql10612503","sql10612503","UfHgGTvcIt",{host:"sql10.freemysqlhosting.net",dialect:"mysql"});
+const db = new Sequelize("sql10612503","sql10612503","UfHgGTvcIt",{host:"sql10.freemysqlhosting.net",dialect:"mysql", dialectModule: require('mysql2')});
 
 module.exports = db;
