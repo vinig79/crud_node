f125103420031bfcfd68a70ff5a388cb17457c04
vinic <viniciusgroto@gmail.com>
2023-04-13 01:15:42 -0300

    config_vercel

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ const User = require("../models/User.js");
 
 
 route.get("/", async (req , res) =>{
-    res.render("login.ejs", {at: req.session.authenticated});
+    res.render("../views/login.ejs", {at: req.session.authenticated});
 })
 
 route.post("/", async (req, res) =>{
