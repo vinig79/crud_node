cbb4924ed1a3f71047fc2c535448ee8423beed0c
vinic <viniciusgroto@gmail.com>
2023-04-13 01:03:47 -0300

    config_vercel

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const sess = {
 };
 
 app.use(express.static('src'));
-app.set("views","../views");
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
