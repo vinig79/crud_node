ddf157653d2e529b713fc082f514f8ea89ea5016
vinic <viniciusgroto@gmail.com>
2023-04-16 22:21:19 -0300

    form

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-const express = require("express");
-const methodOverride = require("method-override");
-const session = require("express-session");
-const db = require("./models/db.js")
-const path = require("path");
+import app from './app.js';
+import db from './src/database/database.js';
 
+console.log(db.models);
 
-const port = 3000;
-const bodyParser = require("body-parser");
+(async () => await db.createTables())();
 
-const app = express();
-
-const index = require("./routes/index.js");
-const register = require("./routes/register.js");
-const edite = require("./routes/edite.js");
-const login = require("./routes/login.js");
-const home = require("./routes/home.js")
-
-const sess = {
-    secret: "secret",
-    cookie: { maxAge: 3000000, secure: false},
-    saveUnitialized : true
-};
-
-app.use(express.static(path.join(__dirname, 'src')));
-app.set('views', path.join(__dirname, 'views'));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.use(session(sess));
-//
-app.use('/',index);
-app.use('/index', index);
-app.use('/register', register );
-app.use('/edite', edite);
-app.use('/login',login);
-app.use("/home", home);
-
-
-app.listen(port, async () =>{
-    await db.sync();
-    console.log("funfo " + port);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`PORT: ${PORT}`));
