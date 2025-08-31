bd45c5e10f15e0ee055f3e2c3e4691f9154be069
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2025-08-31 01:19:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import app from './app.js';
 import db from './src/database/database.js';
 
-// Garante que o banco está preparado na inicialização
 (async () => await db.createTables())();
 
-// Exporta o app para o Vercel tratar como handler
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`PORT: ${PORT}`));
